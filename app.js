e3024a73a229428378bdc51ef2800f37dd343383
JosueJMartinez <jmarti61@gmail.com>
2021-07-08 12:57:03 -0500

    installed dotenv and added boilerplate

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello apples')
+const express = require('express'),
+	dotenv = require('dotenv');
+
+dotenv.config({ path: './configurations/config.env' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+//server listens on here
+app.listen(process.env.PORT || 3000, process.env.IP || '127.0.0.1', () => {
+	console.log(
+		`YelpCamp App started on port: ${process.env.PORT || 3000} at IP: ${
+			process.env.IP || '127.0.0.1'
+		}`
+	);
+});
