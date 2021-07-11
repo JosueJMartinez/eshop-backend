645780926de949fc564e5d8f87151bb20ca2ff6a
JosueJMartinez <jmarti61@gmail.com>
2021-07-10 21:21:22 -0500

    added error handling for database

--- configurations/db.js
+++ configurations/db.js
@@ -2,16 +2,21 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true,
-	});
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
 
-	console.log(
-		`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
-	);
+		console.log(
+			`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+		);
+	} catch (err) {
+		console.log(`Error trying to connect to database`.red.bold);
+		console.log(err);
+	}
 };
 
 module.exports = connectDB;
