89a917ee1faae75affff7fc848b3e2ab33345a04
JosueJMartinez <jmarti61@gmail.com>
2021-10-03 18:54:51 -0500

    refactored logout controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,11 +48,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 //  @route    Get /api/v1/auth/logout
 //  @access   Private
 exports.logout = asyncHandler(async (req, res, next) => {
-	res.cookie('token', 'none', {
-		expires: new Date(Date.now + 3 * 1000),
-		httpOnly: true,
-	});
-	res.status(200).json({ success: true, data: {} });
+	res.clearCookie('token').json({ success: true, data: {} });
 });
 
 //  @desc     Get Current User
