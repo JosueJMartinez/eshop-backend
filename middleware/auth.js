99bdafc14b3e01bcebe878e3ec38a8f90dea6950
JosueJMartinez <jmarti61@gmail.com>
2021-10-15 12:00:53 -0500

    fixed a semantic issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,6 +16,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
 	// Make sure token exists
 	if (!token) throw new ErrorResponse('Not authorized', 401);
+
 	// decode token then use id from decode
 	const decoded = jwt.verify(token, process.env.JWT_SECRET);
 	const userId = decoded.id;
@@ -25,23 +26,27 @@ exports.protect = asyncHandler(async (req, res, next) => {
 	// ===============================================
 
 	// look for it in the Redis Database
-	const data = await redisClient.get(`blacklist_${token}`);
+	const redisData = await redisClient.get(`blacklist_${token}`);
 
 	// if it is on the blacklist tell user to relogin
-	if (data) throw new ErrorResponse('You need to login', 401);
+	if (redisData) throw new ErrorResponse('You need to login again', 401);
 
 	// next check make sure user exists in the database if does assign necessary values to req
 	req.user = await User.findById(userId);
 
-	// We pass the token and tokenExp along in the req for logout
-	req.tokenExp = decoded.exp;
-	req.token = token;
+	// Check if is logout route then
+	// we pass the token and tokenExp along in the req for logout
+	if (req.originalUrl === '/api/v1/auth/logout') {
+		req.tokenExp = decoded.exp;
+		req.token = token;
+	}
 
 	if (!req.user)
 		throw new ErrorResponse(
 			'Uh oh something went wrong logged in user not found',
 			404
 		);
+
 	next();
 });
 
