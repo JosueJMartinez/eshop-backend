bf5e216416dec9e9eeb6b6e845466b67bc218cda
JosueJMartinez <jmarti61@gmail.com>
2021-11-11 13:05:13 -0600

    still working on the date period in calcResults

--- middleware/calcResults.js
+++ middleware/calcResults.js
@@ -14,25 +14,17 @@ const calcResults = input =>
 
 		queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in|sum|match|group)\b/g, match => `$${match}`);
 		query = JSON.parse(queryStr);
-
+		const period = query.period;
 		const arr = [{ $match: query.$match }, { $group: query.$group }];
 		date = arr[0].$match.date;
-		arr[0].$match.date = { $lt: new Date() };
+		arr[0].$match.date = { $lt: new Date(date) };
+		arr[0].$match.date.$gte = new Date(date);
 		if (arr[0].$match.user) arr[0].$match.user = mongoose.Types.ObjectId(arr[0].$match.user);
 		if (arr[0].$match.date) {
-			if (arr[0].$match.period === 'day') {
-				arr[0].$match.date.$gte = new Date(date);
-				arr[0].$match.date.$lt.setDate(date + 1);
-			} else if (arr[0].$match.period === 'week') {
-				arr[0].$match.date.$gte = new Date(date);
-				arr[0].$match.date.$lt.setDate(date + 7);
-			} else if (arr[0].$match.period === 'month') {
-				arr[0].$match.date.$gte = new Date(date);
-				arr[0].$match.date.$lt.setDate(date + 30);
-			} else {
-				arr[0].$match.date.$gte = new Date(date);
-				arr[0].$match.date.$lt.setDate(Date(date + 365));
-			}
+			if (period === 'day') arr[0].$match.date.$lt.setDate(arr[0].$match.date.$lt + 1);
+			else if (period === 'week') arr[0].$match.date.$lt.setDate(arr[0].$match.date.$lt + 7);
+			else if (period === 'month') arr[0].$match.date.$lt.setDate(arr[0].$match.date.$lt + 30);
+			else arr[0].$match.date.$lt.setDate(arr[0].$match.date.$lt + 365);
 		}
 		console.log(arr[0].$match);
 		const result = await Model.aggregate([arr]);
